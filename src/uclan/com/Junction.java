a0c58912edfaeefc887760d6eb1c5719abab99bb
Ttan-24 <47601502+Ttan-24@users.noreply.github.com>
2021-12-11 20:23:21 +0000

    Added Junction attributes

--- src/uclan/com/Junction.java
+++ src/uclan/com/Junction.java
@@ -1,5 +1,18 @@
 package uclan.com;
 
 public class Junction {
+	Boolean Route;
+	// array of route
+	// array of 4 entrances
+	// array of 4 exits
+	// boolean array of 4 enables - flags and toggle
+	int entranceIndex;
 
+//array of four different durations for green lights
+
+	void simulateCars() {
+// wait
+//ask
+//sleep
+	}
 }
